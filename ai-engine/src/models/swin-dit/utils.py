14cf97ef7edbbe3f8c345feb562738581534a1d8
Aatti13 <aattreya.ks@gmail.com>
2026-01-27 22:41:55 +0530

    Added Utility Functions

---
+++ ai-engine/src/models/swin-dit/utils.py
@@ -0,0 +1,14 @@
+import torch
+from typing import Tuple
+
+def window_partition(x: torch.Tensor, window_size: int) -> Tuple[torch.Tensor, int, int]:
+  B, H, W, C = x.shape
+  x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+  windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+  return windows, H, W
+
+def window_reverse(windows: torch.Tensor, window_size: int, H: int, W: int)-> torch.Tensor:
+  B = int(windows.shape[0] / (H * W / window_size / window_size))
+  x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+  x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+  return x
